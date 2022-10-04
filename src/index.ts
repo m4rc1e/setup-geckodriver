a42ca9fa6126d0ddb0e2074a1dd6d096d485bb79
Marc Foley <m.foley.88@gmail.com>
2022-10-04 11:29:27 +0100

    do not trigger request yet

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,11 @@ type GitHubReleaseApiResponse = {
 };
 
 const getLatestVersion = async (): Promise<string> => {
+  const githubToken = core.getInput("github-token");
   const apiURL = `https://api.github.com/repos/mozilla/geckodriver/releases/latest`;
-  const http = new httpm.HttpClient("setup-geckodrive")
+  const http = new httpm.HttpClient("setup-geckodrive", [],{})// {
+//    headers: {"authorization": `bearer {githubToken}`}
+//  })
   const resp = await http.getJson<GitHubReleaseApiResponse>(apiURL);
   if (resp.statusCode !== httpm.HttpCodes.OK) {
     throw new Error(
